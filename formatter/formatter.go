95833dc234dbaa714bfaa6438930afd9433a928d
Kyle Gordon <kygordon@microsoft.com>
2021-08-06 10:35:48 -0700

    Updating imports for local use

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/jstemmer/go-junit-report/parser"
+	"github.com/SirAlvarex/go-junit-report/parser"
 )
 
 // JUnitTestSuites is a collection of JUnit test suites.
